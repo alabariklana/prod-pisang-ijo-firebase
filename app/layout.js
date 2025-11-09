6a2327b545b5abc476442df2e0385c6b84f35012
alabariklana <alaunasbariklana>
2025-11-09 12:53:52 +0800

    feat: Add Google Tag Manager (GTM-NHP3VZXC) integration
    - Add GTM script to <head> section for tracking initialization
    - Add GTM noscript fallback after <body> tag for users with disabled JavaScript
    - Implement proper dangerouslySetInnerHTML for Next.js compatibility
    - Enable comprehensive analytics and conversion tracking across the website

--- app/layout.js
+++ app/layout.js
@@ -123,6 +123,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NHP3VZXC');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -132,6 +143,16 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={`${playfairDisplay.variable} ${poppins.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NHP3VZXC"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Toaster position="top-center" richColors />
       </body>
