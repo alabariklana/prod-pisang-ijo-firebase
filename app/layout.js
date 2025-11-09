730153ea874b7dd49eaddfdce7f824744f426e2c
alabariklana <alaunasbariklana>
2025-11-09 14:03:01 +0800

    feat: Replace Google Tag Manager with Google Analytics 4 (gtag.js)
    - Remove old GTM implementation (GTM-NHP3VZXC)
    - Add new Google Analytics 4 tracking (G-20SQV22G7L)
    - Implement gtag.js for better performance and direct GA4 integration
    - Remove unnecessary noscript fallback as gtag.js handles fallbacks internally
    - Cleaner implementation with async loading for optimal page performance

--- app/layout.js
+++ app/layout.js
@@ -123,17 +123,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-20SQV22G7L" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NHP3VZXC');`
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-20SQV22G7L');
+            `
           }}
         />
-        {/* End Google Tag Manager */}
+        {/* End Google tag */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -143,16 +145,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body className={`${playfairDisplay.variable} ${poppins.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NHP3VZXC"
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         {children}
         <Toaster position="top-center" richColors />
       </body>
